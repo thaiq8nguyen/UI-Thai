6bade2e92895e16e7eb926c0bc5ecec2c82b91ee
Thai Nguyen <discoverylight@yahoo.com>
2019-06-25 16:09:15 -0700

    added an about page with team images

--- src/about.js
+++ src/about.js
@@ -1,11 +1,14 @@
 import "@fortawesome/fontawesome-free/js/all"
-import "./styles/index.less"
+import "./styles/about.less"
 
 import Header from "./components/Header/Header.js";
 import SideMenu from "./components/SideMenu/SideMenu.js";
+import Team from "./components/Team/Team.js";
 
 const headerContainer = document.querySelector(".header");
 const sideMenuContainer = document.querySelector(".side-menu");
+const teamContainer = document.querySelector(".team");
 
 new Header(headerContainer, sideMenuContainer);
 new SideMenu(sideMenuContainer);
+new Team(teamContainer);
---
+++ src/components/Team/Team.js
@@ -0,0 +1,7 @@
+class Team {
+    constructor(teamElement) {
+        this.element = teamElement;
+    }
+}
+
+export default Team;
