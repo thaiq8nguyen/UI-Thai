b941d66dbc3a10d3cfaf2b1d481437ecf016cf28
Thai Nguyen <discoverylight@yahoo.com>
2019-06-27 11:36:54 -0700

    added axios to packages add current donations live data card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,16 +1,69 @@
+import axios from "axios";
 
 class Card {
     constructor(cardElement) {
         this.element = cardElement;
-        if(this.element.getAttribute("id") === "donate-today") {
-            //this.animateDonateToday();
+        if(this.element.getAttribute("id") === "current-donation") {
+            this.getCurrentDonation();
         }
 
     }
 
-    animateDonateToday() {
-        const header = this.element.querySelector("h1");
-        header.style.borderTop = "0.4rem solid trasparent";
+    async getCurrentDonation() {
+        try {
+
+            const totalDonationAmounts = await axios.get("https://donor-manage-bw.herokuapp.com/api/donations/total");
+            
+            const totalDonationCounts = await axios.get("https://donor-manage-bw.herokuapp.com/api/donations/count");
+
+            const totalDonorCounts = await axios.get("https://donor-manage-bw.herokuapp.com/api/donor/count")
+
+            
+            
+
+            this.displayDonations([totalDonationAmounts, totalDonationCounts, totalDonorCounts])
+
+        } catch(error) {
+            this.displayError(error);   
+        }
+        
+        
+    }
+
+    displayDonations(rawDonations) {
+        const dataContainer = this.element.querySelector(".data-content");
+        const names = ["Total Donation Amounts", "Total Donation Counts", "Total Donor Counts"];
+
+        let template = "";
+        
+        let donations = rawDonations.map((rawDonation, index) => {
+            
+            if(index === 0) {
+                return "$" + this.formatNumber(rawDonation.data.total)
+            } else {
+                return rawDonation.data.total;  
+            }
+
+        })
+
+
+        donations.forEach((donation, index) => {
+            template += `<div class="donations">
+            <span class="names">${names[index]}</span>
+            <span class="stats">${donation}</span>
+            </div>`    
+        })
+        
+        dataContainer.innerHTML = template;
+        
+    }
+
+    displayError(error) {
+
+    }
+
+    formatNumber(num) {
+        return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
     }
 }
 
