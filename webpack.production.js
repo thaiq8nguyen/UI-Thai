194b7cd4ef74f5fd855f9c6a2fbee810f4617b8a
Thai Nguyen <discoverylight@yahoo.com>
2019-06-26 20:28:24 -0700

    preproduction deploy

---
+++ webpack.production.js
@@ -0,0 +1,82 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: {
+        index: "./src/index.js",
+        about: "./src/about.js"
+    },
+    output: {
+        filename: "[name]-bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    
+    module: {
+        rules: [
+            
+            {
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: "less-loader"
+                    }
+                    
+                ]
+            },
+            {   
+                test: /\.(jpg|png|jpeg)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "./images/[name].[ext]"
+                        }
+                    }
+                ]
+
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "./fonts/[name].[ext]"
+                        }
+                        
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                loader: "html-loader"
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            filename: "index.html",
+            title: "Donor Management",
+            template: "./src/index.html",
+            chunks: ["index"]
+
+        }),
+        new HTMLWebpackPlugin({
+            filename: "about.html",
+            title: "About",
+            template: "./src/about.html",
+            chunks: ["about"]
+        }),
+        new MiniCssExtractPlugin()
+
+        
+    ]
+    
+}
